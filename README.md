f770116dda9ebaeeb77b747aee0f0e71a3b534ac
luiscaputo0 <andersonfranciscoig@gmail.com>
2024-06-10 11:02:07 +0100

    feat: update estr

--- README.md
+++ README.md
@@ -9,4 +9,17 @@
 
 ## Para o aquivo main.py vamos precisar instar a seguinte biblioteca:
 
-** pip install camelot-py[cv] pandas
+** pip install camelot-py\[cv\] pandas 
+** pip install 'camelot-py[cv]' pandas
+
+
+## Ao executar o programa usando o comando python3 main.py ele vai retornar o seguinte:
+
+0    Nome      Idade     Cidade
+0    João         12     Luanda
+1   Maria         20   Talatona
+2   Pedro         50      Samba
+3    Pais  Província  Município
+4  Angola     Luanda   Talatona
+5  Angola     Luanda    Cacuaco
+6  Angola     Luanda      Viana
--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-import tabula
+import camelot
 import pandas as pd
 
-# Carregar o arquivo PDF
-file_path = "caminho/para/o/arquivo.pdf"
+file_path = "TesteDataFrame.pdf"
 
-# Extrair tabelas do PDF
-tables = tabula.read_pdf(file_path, pages='all')
+try:
+    tables = camelot.read_pdf(file_path, pages='all', flavor='stream')
 
-# Converter as tabelas em DataFrames
-dfs = []
-for table in tables:
-    df = pd.DataFrame(table)
-    dfs.append(df)
+    if not tables:
+        print("Nenhuma tabela encontrada no arquivo PDF.")
+    else:
+        dfs = [table.df for table in tables]
+        final_df = pd.concat(dfs, ignore_index=True)
 
-final_df = pd.concat(dfs, ignore_index=True)
+        first_row = final_df.iloc[0]
+        if any(pd.notna(first_row)):
+            final_df.columns = first_row
+            final_df = final_df.iloc[1:].reset_index(drop=True)
+        else:
+            num_columns = len(final_df.columns)
+            final_df.columns = [f"Coluna{i+1}" for i in range(num_columns)]
 
-print(final_df)
+        print(final_df)
+
+except Exception as e:
+    print(f"Erro ao processar o PDF: {e}")
+
+
